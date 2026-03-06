8bb0be53ae365b1151dfe99e8c81b9fc235213dd
Azzu1997 <shaikazmat12345@gmal.com>
2026-03-06 09:32:39 +0530

    Added browsers

--- src/test/java/Practice/task.java
+++ src/test/java/Practice/task.java
@@ -1,9 +1,28 @@
 package Practice;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
 public class task
 {
-public static void main(String[] args)
+	WebDriver driver;
+@Parameters("browser")
+@Test	
+public void main(String BROWSER)
 {
+	BROWSER =System.getProperty("browser");
+	if(BROWSER.equalsIgnoreCase("chrome"))
+	{
+		driver =new ChromeDriver();
+	}
+	else if(BROWSER.equalsIgnoreCase("firefox"))
+	{
+		driver =new FirefoxDriver();
+	}
+	
 	System.out.println("done the task");
 	
 }
